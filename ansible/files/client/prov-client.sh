68e2ea4a6b0c0abaaf5fac999be516ccffc17708
nussnk <nuss@yandex.ru>
2023-09-24 01:51:53 +0300

    working 1.0

--- ansible/files/client/prov-client.sh
+++ ansible/files/client/prov-client.sh
@@ -5,7 +5,7 @@ chown borg:borg /home/borg/.ssh/id_rsa
 
 cat /vagrant/keys/borg.key >> /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
-chown borg:borg /root/.ssh/id_rsa
+chown root:root /root/.ssh/id_rsa
 # initialisation of borg
 #borg init --encryption=repokey borg@192.168.11.160:/var/backup
 
